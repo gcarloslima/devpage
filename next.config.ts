631667ad4a9d8537e97e36996e98144e664ab912
gcarloslima <carlosrelgs@gmail.com>
2025-03-09 14:47:51 -0300

    Corrigindo tipo do darkMode

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/devpage",
+  assetPrefix: "/devpage/",
 };
 
 export default nextConfig;
